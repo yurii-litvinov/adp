1a31cdda03ed59a33c745e5347a466a5883a566d
yurii-litvinov <yurii.litvinov@gmail.com>
2018-06-24 14:13:44 +0300

    Added some parallelism for PDF reading

--- ADP/PdfInfoExtractor.fs
+++ ADP/PdfInfoExtractor.fs
@@ -6,6 +6,7 @@ module PdfInfoExtractor =
     open System.IO
     open System.Text.RegularExpressions
 
+    /// Possible names of chairs and education programs that have to appear on the top of a title page.
     let private programs = [
         "Кафедра системного программирования";
         "Кафедра cистемного программирования";  // That's not the same string. Unicode is kind of insane.
@@ -22,20 +23,23 @@ module PdfInfoExtractor =
         "университет»"
     ]
 
+    /// Symbols that are allowed to be in the title.
     let private allowedTitleSymbols = [@"\w"; @"\s"; @"\n"; "\p{P}"]
 
-    let private worksKind = [
+    /// List of known qualification work kinds, it shall appear in the middle of a title page.
+    let private workKinds = [
         "Выпускная";
         "Бакалаврская"
         "Дипломная";
         "Курсовая";
     ]
 
+    /// Creates a list regexp patterns that will be matched against extracted text.
     let private buildPatterns () =
         let alt list = list |> List.reduce (fun s1 s2 -> s1 + "|" + s2)
         let titlePattern = alt allowedTitleSymbols
         let titlePattern = "(?<Title>(" + titlePattern + ")+)"
-        let worksPattern = "(" + (alt worksKind) + ")"
+        let worksPattern = "(" + (alt workKinds) + ")"
         let regularPatterns = 
             programs 
             |> List.map (fun s -> s + @"\s*\n+(?<Name>\w+\s\w+\s\w+)\s*\n+" + titlePattern + @"\n+" + worksPattern)
@@ -44,6 +48,8 @@ module PdfInfoExtractor =
             |> List.map (fun s -> s + @"\s*\n+(?<Name>\w+\s\w+\s\w+)\s*\n+" + titlePattern + @"\n+" + "Научный руководитель")
         regularPatterns @ workKindOmittedPatterns
  
+    /// Tries to open given file as a PDF document and extract all possible information from it and put it into given
+    /// Diploma record.
     let private extractFromPdf (diploma: Diploma) (file: string) =
         let text = PdfTextExtractor.getFirstPageText file
         let regexps = buildPatterns () |> List.map (fun pattern -> Regex(pattern, RegexOptions.IgnoreCase))
@@ -71,18 +77,23 @@ module PdfInfoExtractor =
             let name = advisorMatch.Groups.["Advisor"].Value
             diploma.AdvisorName <- name
 
+    /// Checks that given diploma has PDF document with report and then tries to extract information from it.
     let private extractForDiploma (diploma: Diploma) =
-        if not diploma.HasText then
-            ()
-        else
-            let file = diploma.Text.Value.FileName
-            if FileInfo(file).Extension <> ".pdf" then 
+        async {
+            if not diploma.HasText then
                 ()
             else
-                extractFromPdf diploma file 
+                let file = diploma.Text.Value.FileName
+                if FileInfo(file).Extension <> ".pdf" then 
+                    ()
+                else
+                    extractFromPdf diploma file 
+        }
 
     /// Iterates all known works in a base, trying to look inside a report .pdf file and extract title and author name.
     /// Results are stored again in a knowledge base.
     let extract (knowledgeBase: KnowledgeBase) =
-        knowledgeBase.AllWorks |> Seq.iter extractForDiploma
+        // All parallel chunks of this work actually modify the same knowledge base, but no race conditions are 
+        // possible since every thread modifies its own diploma and they are guaranteed to be unique.
+        knowledgeBase.AllWorks |> Seq.map extractForDiploma |> Async.Parallel |> Async.RunSynchronously |> ignore
         knowledgeBase
